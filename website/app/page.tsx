f0a67ba16aac43d9a260b27fbe0fd8ae5e1be228
Sophia Don Tranho <66497192+sophiadt@users.noreply.github.com>
2025-06-12 23:52:32 -0700

    centerish

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <div>
+    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <h1>StreetScope.ai</h1>
         <p>For data-driven cities</p>
       </div>
