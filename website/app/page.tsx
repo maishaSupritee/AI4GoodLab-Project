abdaf02a36fb3e50c8e33c5caa9e849a45b32306
mariaKapitan <mkapitanenko@gmail.com>
2025-06-13 12:15:50 -0700

    hi im new here

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full text-center flex flex-col justify-center items-center gap-5 my-10">
-        <h1 className="text-4xl sm:text-6xl font-bold text-purple-600 my-5">
+        <h1 className="text-4xl sm:text-6xl font-bold text-purple-200 my-5">
           StreetScope.ai
         </h1>
         <p className="text-lg sm:text-xl text-gray-700">
