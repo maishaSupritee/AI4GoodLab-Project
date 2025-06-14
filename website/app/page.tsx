7d9890cd31f661c2f18a78ee7a285cbd1a183fb8
mariaKapitan <mkapitanenko@gmail.com>
2025-06-14 15:48:01 -0700

    i added a logo!

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,17 +1,27 @@
 //home page for website
+import Image from "next/image";
 import Map from "@/components/Map";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full text-center flex flex-col justify-center items-center gap-5 my-10">
-        <h1 className="text-4xl sm:text-6xl font-bold text-purple-200 my-5">
-          StreetScope.ai
-        </h1>
-        <p className="text-lg sm:text-xl text-gray-700">
-          A platform for data-driven urban planning
-        </p>
-      </div>
+    <main className="mx-auto sm:px-10 px-5 flex flex-col">
+      <header className="w-full flex items-center justify-start gap-4 py-6">
+        <Image
+          src="/streetscope.png"
+          alt="StreetScope.ai Logo"
+          width={100}
+          height={100}
+          className="flex-shrink-0"
+        />
+        <div className="flex flex-col">
+          <h1 className="text-2xl sm:text-4xl font-montserrat text-[#373584] font-bold">
+            StreetScope.ai
+          </h1>
+          <p className="text-lg sm:text-xl text-gray-700">
+            A platform for data-driven urban planning
+          </p>
+        </div>
+      </header>
 
       <div className="w-full h-[500px]">
         <Map />
