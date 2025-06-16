96f8786a0055935ae57bb60659e25bc32f37abb4
Sophia Don Tranho <66497192+sophiadt@users.noreply.github.com>
2025-06-16 02:29:17 -0700

    example select from react

--- website/components/Map.tsx
+++ website/components/Map.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
+"use client";
+
+import React, { use } from 'react'
+import { useState } from 'react';
 
 const Map = () => {
+  const [view, setView] = useState("layered_map");
+
   return (
-    <div className="w-full h-[600px] border border-gray-700 rounded-lg overflow-hidden">
-      <iframe
-        src="/maps/map.html"
-        width="100%"
-        height="100%"
-        title="Folium Map with Layer Controls"
-        style={{ border: "none" }}
-      ></iframe>
+    <div>
+      <select onChange={(e) => setView(e.target.value)} className="mb-4">
+        <option value="layered_map">Base Map</option>
+        <option value="layered_sidewalks">Sidewalk Improvements</option>
+        <option value="layered_lighting">Lighting Upgrades</option>
+      </select>
+      <div className="w-full h-[600px] border border-gray-700 rounded-lg overflow-hidden">
+        <iframe
+          src="/maps/map.html"
+          width="100%"
+          height="100%"
+          title="Folium Map with Layer Controls"
+          style={{ border: "none" }}
+        ></iframe>
+      </div>
     </div>
   )
 }
