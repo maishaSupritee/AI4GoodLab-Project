168f68d1028ee3354338de1d49433b4de20f8453
mariaKapitan <mkapitanenko@gmail.com>
2025-06-16 23:52:45 -0700

    Shapes and colours!

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -5,7 +5,8 @@ import Map from "@/components/Map";
 export default function Home() {
   return (
     <main className="mx-auto sm:px-10 px-5 flex flex-col">
-      <header className="w-full flex items-center justify-start gap-4 py-6">
+      <header className="w-full flex items-center justify-between gap-4 py-6">
+      <div className="flex items-center gap-4">
         <Image
           src="/streetscope.png"
           alt="StreetScope.ai Logo"
@@ -17,12 +18,24 @@ export default function Home() {
           <h1 className="text-2xl sm:text-4xl font-montserrat text-[#373584] font-bold">
             StreetScope.ai
           </h1>
-          <p className="text-lg sm:text-xl text-gray-700">
+          <h2 className="text-lg sm:text-xl text-gray-700">
             A platform for data-driven urban planning
-          </p>
+          </h2>
+        </div>
+        </div>
+      
+      <div className="flex items-center gap-3">
+          <button className="px-4 py-2 bg-[#373584] text-white rounded hover:bg-opacity-300">
+            How to Use
+          </button>
+          <button className="px-4 py-2 bg-orange-300 text-gray-800 rounded hover:bg-gray-300">
+            Mission
+          </button>
+          <button className="px-4 py-2 bg-orange-300 text-gray-800 rounded hover:bg-gray-300">
+            About Us
+          </button>
         </div>
       </header>
-
       <div className="w-full h-[500px]">
         <Map />
       </div>
