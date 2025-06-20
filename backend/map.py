e9711912a87146b39d69ea59f8ca70d48e5d1bb1
mariaKapitan <mkapitanenko@gmail.com>
2025-06-19 23:51:03 -0700

    Hot Map

--- backend/map.py
+++ backend/map.py
@@ -4,7 +4,7 @@ import folium
 from folium.plugins import HeatMap
 
 csv_path = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "fake_data.csv")
+    os.path.join(os.path.dirname(__file__), "output1.csv")
 )
 df = pd.read_csv(csv_path)
 
@@ -19,8 +19,11 @@ folium.Marker(location=(49.22691073805821, -123.12734878115373), popup = "Vancou
 # folium.LayerControl(collapsed=False).add_to(m)
 
 #heat map
-heat_data = df[["latitude", "longitude", "weight"]].values.tolist()
-hm = HeatMap(heat_data, min_opacity=0.05, max_opacity=0.8, radius=15, blur=10, gradient={0.2: 'purple', 0.4: 'yellow', 0.8: 'orange'})
+weight_map = {1:7, 2:6, 3:5, 4:4, 5:3, 6:2, 0:1} #made up classification weights for clusters, 1 = worst, 7 = best
+df['weight'] = df['k=7 clustering'].map(weight_map)
+heat_data = df[["latitude", "longitude", "weight"]].dropna().values.tolist()
+hm = HeatMap(heat_data, min_opacity=0.05, max_opacity=1.0, radius=15, blur=20, gradient={0.2: 'blue', 0.4: 'cyan', 0.6: 'lime', 0.8: 'yellow', 1.0: 'red'})   
+
 hm.add_to(m)
 
 folium.LayerControl().add_to(m)
